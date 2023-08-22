088347a3b4ccdf5f6a81d47cc236a1dc6d044b53
Anish V <anishv3124@gmail.com>
2023-08-23 00:17:39 +0530

    added UI

--- Student_visualise/views/display.php
+++ Student_visualise/views/display.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Student Information</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
+
 <body>
     <div class="container">
         <h1>Student Results</h1>
@@ -27,42 +29,59 @@
         </div>
     </div>
     <div id="bar-graph">
-    <canvas id="marksChart"></canvas>
-    <script>
-    var marksChart = new Chart(document.getElementById("marksChart"), {
-        type: 'bar',
-        data: {
-            labels: ['Subject 1', 'Subject 2', 'Subject 3'],
-            datasets: [
-                {
-                    label: 'Your Marks',
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1,
-                    data: [<?php echo $mark1; ?>, <?php echo $mark2; ?>, <?php echo $mark3; ?>]
-                },
-                {
-                    label: 'Max Marks',
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1,
-                    data: [
-                        <?php echo $maxMarksBySubject['max_mark1']; ?>,
-                        <?php echo $maxMarksBySubject['max_mark2']; ?>,
-                        <?php echo $maxMarksBySubject['max_mark3']; ?>
+        <canvas id="marksChart"></canvas>
+        <script>
+            var marksChart = new Chart(document.getElementById("marksChart"), {
+                type: 'bar',
+                data: {
+                    labels: ['Subject 1', 'Subject 2', 'Subject 3'],
+                    datasets: [{
+                            label: 'Your Marks',
+                            backgroundColor: 'rgb(51, 102, 204)',
+                            borderColor: 'rgba(256, 228, 196, 1)',
+                            borderWidth: 1,
+                            data: [<?php echo $mark1; ?>, <?php echo $mark2; ?>, <?php echo $mark3; ?>]
+                        },
+                        {
+                            label: 'Max Marks',
+                            backgroundColor: 'rgba(256, 228, 196, 1)',
+                            borderColor: 'rgb(51, 102, 204)',
+                            borderWidth: 1,
+                            data: [
+                                <?php echo $maxMarksBySubject['max_mark1']; ?>,
+                                <?php echo $maxMarksBySubject['max_mark2']; ?>,
+                                <?php echo $maxMarksBySubject['max_mark3']; ?>
+                            ]
+                        }
                     ]
-                }
-            ]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            labels: {
+                                color: 'white' // Set label color to white
+                            }
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            ticks: {
+                                color: 'white' // Set the tick label color to white
+                            }
+                        },
+                        x: {
+                            beginAtZero: true,
+                            ticks: {
+                                color: 'white' // Set the tick label color to white
+                            }
+                        }
+                    }
                 }
             }
-        }
-    });
-</script>
+            );
+        </script>
     </div>
 </body>
+
 </html>
--- Student_visualise/views/home.php
+++ Student_visualise/views/home.php
@@ -1,34 +1,47 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Student Information Form</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style2.css">
 </head>
+
 <body>
-    <h1>Student Information Form</h1>
-    <form action="index.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br>
+    <header>
+        <div>
+            <!-- <img src="Student_visualise\views\bg_image.jpg"> -->
+            <div class="flex-container">
+                <h1><b>Student Information Form</b></h1>
+            </div>
+    </header>
+    <div class="inp">
+        <form action="index.php" method="post">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required><br>
 
-        <label for="dob">Date of Birth:</label>
-        <input type="date" id="dob" name="dob" required><br>
+            <label for="dob">Date of Birth:</label>
+            <input type="date" id="dob" name="dob" required><br>
 
-        <label for="age">Age:</label>
-        <input type="number" id="age" name="age" required><br>
+            <label for="age">Age:</label>
+            <input type="number" id="age" name="age" required><br>
 
-        <label for="reg_no">Registration Number:</label>
-        <input type="text" id="reg_no" name="reg_no" required><br>
+            <label for="reg_no">Registration Number:</label>
+            <input type="text" id="reg_no" name="reg_no" required><br>
 
-        <label for="mark1">Mark 1:</label>
-        <input type="number" id="mark1" name="mark1" required><br>
+            <label for="mark1">Mark 1:</label>
+            <input type="number" id="mark1" name="mark1" required><br>
 
-        <label for="mark2">Mark 2:</label>
-        <input type="number" id="mark2" name="mark2" required><br>
+            <label for="mark2">Mark 2:</label>
+            <input type="number" id="mark2" name="mark2" required><br>
 
-        <label for="mark3">Mark 3:</label>
-        <input type="number" id="mark3" name="mark3" required><br>
+            <label for="mark3">Mark 3:</label>
+            <input type="number" id="mark3" name="mark3" required><br>
 
-        <button type="submit">Submit</button>
-    </form>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+    </div>
 </body>
+
 </html>
